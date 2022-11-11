3c853b524542a9068cbb17df6576079da960ab5b
sadnanrakib <sadnanrakib02@gmail.com>
2022-11-11 17:51:46 +0600

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsk4obg.mongodb.net/?retryWrites=true&w=majority`;
 console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_PASSWORD);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
  try{
